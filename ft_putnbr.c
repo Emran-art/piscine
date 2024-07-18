6d1ca4e4020b54c78f3b2828e94e8a9b81b49917
Emran <75085344+Emran-art@users.noreply.github.com>
2024-07-18 14:08:51 +0300

    C04

---
+++ ft_putnbr.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eahmad <eahmad@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/10 12:41:57 by eahmad            #+#    #+#             */
+/*   Updated: 2024/07/10 12:42:02 by eahmad           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+
+{
+	if (nb == -2147483648)
+	{
+		ft_putchar('-');
+		ft_putchar('2');
+		ft_putnbr(147483648);
+	}
+	else if (nb < 0)
+	{
+		ft_putchar('-');
+		nb = -nb;
+		ft_putnbr(nb);
+	}
+	else if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+		ft_putchar(nb + 48);
+}
+int	main()
+{
+	ft_putnbr(2147483649);
+	return (0);
+}
